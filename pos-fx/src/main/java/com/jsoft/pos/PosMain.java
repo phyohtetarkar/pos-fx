4c18457b2ea5207173158570ed8fcbb14650d722
phyohtetarkar <phyohtetarkar@cartoonkid>
2017-09-19 22:31:46 +0630

    name change

--- pos-fx/src/main/java/com/jsoft/pos/PosMain.java
+++ pos-fx/src/main/java/com/jsoft/pos/PosMain.java
@@ -11,7 +11,7 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-public class ApplicationMain extends Application {
+public class PosMain extends Application {
 
 	@Override
 	public void start(Stage stage) throws Exception {
